f831507b91c06ba8894fc6710ef9e051705d49b9
Alex <alexbacirea@gmail.com>
2023-11-30 02:23:00 +0200

    add: select collections for mobile

--- components/mobile-collections.tsx
+++ components/mobile-collections.tsx
@@ -4,29 +4,36 @@ import { Fragment, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import CheckIcon from './icons/check';
 import CaretUpDownIcon from './icons/caret-up-down';
+import { useRouter } from 'next/navigation';
 
 const collections = [
-  { name: 'All' },
-  { name: 'Phones' },
-  { name: 'Sweaters' },
-  { name: 'Drinkware' },
-  { name: 'Hats' },
-  { name: 'Drones' },
-  { name: 'Earbuds' },
+  { name: 'All', url: '/search' },
+  { name: 'Phones', url: '/search/phones' },
+  { name: 'Sweaters', url: '/search/sweaters' },
+  { name: 'Drinkware', url: '/search/drinkware' },
+  { name: 'Hats', url: '/search/hats' },
+  { name: 'Drones', url: '/search/drones' },
+  { name: 'Earbuds', url: '/search/earbuds' },
 ];
 
 export default function MobileCollections() {
   const [selected, setSelected] = useState(collections[0]);
+  const router = useRouter();
+
+  function handleChangeURL(option: any) {
+    setSelected(option);
+    router.push(option.url);
+  }
 
   return (
     <>
-      <Listbox value={selected} onChange={setSelected}>
+      <Listbox value={selected} onChange={handleChangeURL}>
         <div className="relative mt-1 w-full">
-          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-blue-50 py-2 pl-3 pr-10 text-left shadow focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-blue-300 sm:text-sm">
             <span className="block truncate">{selected.name}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <CaretUpDownIcon
-                classname="h-5 w-5 text-gray-400"
+                classname="h-5 w-5 text-gray-500"
                 aria-hidden="true"
               />
             </span>
@@ -37,16 +44,16 @@ export default function MobileCollections() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-              {collections.map((person, personIdx) => (
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-lg bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {collections.map((item, itemIdx) => (
                 <Listbox.Option
-                  key={personIdx}
+                  key={itemIdx}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-blue-100 text-blue-900' : 'text-gray-900'
+                      active ? 'bg-blue-100 text-blue-900' : 'text-gray-950'
                     }`
                   }
-                  value={person}
+                  value={item}
                 >
                   {({ selected }) => (
                     <>
@@ -55,7 +62,7 @@ export default function MobileCollections() {
                           selected ? 'font-medium' : 'font-normal'
                         }`}
                       >
-                        {person.name}
+                        {item.name}
                       </span>
                       {selected ? (
                         <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600">
