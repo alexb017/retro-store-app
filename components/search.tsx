921ad6d881e6ec2c77a5c846592bf8d839bca404
Alex <alexbacirea@gmail.com>
2024-11-06 17:57:57 +0200

    fix(search): set input value to default when pathname changes

--- components/search.tsx
+++ components/search.tsx
@@ -4,7 +4,7 @@ import {
   useRouter,
   useSearchParams,
 } from 'next/navigation';
-import React from 'react';
+import { useState, useEffect } from 'react';
 import MagnifyingIcon from './icons/magnifying';
 import { Input } from './ui/input';
 import { useDebouncedCallback } from 'use-debounce';
@@ -13,6 +13,13 @@ export default function Search() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const pathname = usePathname();
+  const [searchValue, setSearchValue] = useState(
+    searchParams.get('q')?.toString() || ''
+  );
+
+  useEffect(() => {
+    setSearchValue(searchParams.get('q')?.toString() || '');
+  }, [pathname]);
 
   // It creates a new URLSearchParams object with the input value
   // It uses the useDebouncedCallback hook to debounce the search function
@@ -38,9 +45,10 @@ export default function Search() {
         placeholder="Search for products..."
         className="block w-full rounded-full bg-zinc-100 hover:bg-zinc-200 focus-visible:bg-zinc-200 border-none py-[9px] pl-10 text-sm placeholder:text-zinc-500 focus-visible:ring-offset-0 focus-visible:ring-0 dark:placeholder:text-zinc-400 dark:bg-zinc-900 dark:hover:bg-zinc-800 transition-colors dark:focus-visible:bg-zinc-800"
         onChange={(e) => {
+          setSearchValue(e.target.value);
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('q')?.toString()}
+        value={searchValue || ''}
       />
       <MagnifyingIcon classname="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-zinc-500" />
     </div>
